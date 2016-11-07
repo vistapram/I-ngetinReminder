71a4a35f3837e9982d715c9f001f828779df0c81
arumdeun <arumdeun66@gmail.com>
2016-11-08 05:00:09 +0700

    Setel jam (Timepicker).

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl102122232/ingetinreminder/settime.java
@@ -0,0 +1,61 @@
+package id.sch.smktelkom_mlg.project.xiirpl102122232.ingetinreminder;
+
+import android.app.TimePickerDialog;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends Activity {
+    private TimePicker timePicker1;
+    private TextView time;
+    private Calendar calendar;
+    private String format = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+        time = (TextView) findViewById(R.id.textView1);
+        calendar = Calendar.getInstance();
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int min = calendar.get(Calendar.MINUTE);
+        showTime(hour, min);
+    }
+
+    public void setTime(View view) {
+        int hour = timePicker1.getCurrentHour();
+        int min = timePicker1.getCurrentMinute();
+        showTime(hour, min);
+    }
+
+    public void showTime(int hour, int min) {
+        if (hour == 0) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+
+        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
+                .append(" ").append(format));
+    }
+
+
+}
+
