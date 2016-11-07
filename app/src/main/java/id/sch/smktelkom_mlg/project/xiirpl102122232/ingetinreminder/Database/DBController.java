be2213907b36a599cc6663a12d4851b25b50ff72
vistapram <vistapramudya@gmail.com>
2016-11-07 18:01:16 +0700

    +Design Navigation Drawer +DB controller & DBhelper

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl102122232/ingetinreminder/Database/DBController.java
@@ -0,0 +1,68 @@
+package id.sch.smktelkom_mlg.project.xiirpl102122232.ingetinreminder.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Vistapram on 2016-11-07.
+ */
+public class DBController {
+
+    private DBHelper dbHelper;
+    private Context ourcontext;
+    private SQLiteDatabase database;
+
+    public DBController(Context c) {
+        ourcontext = c;
+    }
+
+    public DBController open() throws SQLException {
+        dbHelper = new DBHelper(ourcontext);
+        database = dbHelper.getWritableDatabase();
+        return this;
+
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public void insert(String name, String tanggal, String waktu, String file, String ket) {
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(DBHelper.ROW_NAMAKEG, name);
+        contentValue.put(DBHelper.ROW_TGLKEG, tanggal);
+        contentValue.put(DBHelper.ROW_WAKTU, waktu);
+        contentValue.put(DBHelper.ROW_FILE, file);
+        contentValue.put(DBHelper.ROW_KET, ket);
+        database.insert(DBHelper.TABLE_NAME, null, contentValue);
+    }
+
+    public Cursor fetch() {
+        String[] columns = new String[]{DBHelper.ROW_ID, DBHelper.ROW_NAMAKEG, DBHelper.ROW_TGLKEG, DBHelper.ROW_WAKTU, DBHelper.ROW_FILE,
+                DBHelper.ROW_KET};
+        Cursor cursor = database.query(DBHelper.TABLE_NAME, columns, null,
+                null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public int update(long _id, String name, String tanggal, String waktu, String file, String ket) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBHelper.ROW_NAMAKEG, name);
+        contentValues.put(DBHelper.ROW_TGLKEG, tanggal);
+        contentValues.put(DBHelper.ROW_WAKTU, waktu);
+        contentValues.put(DBHelper.ROW_FILE, file);
+        contentValues.put(DBHelper.ROW_KET, ket);
+        int i = database.update(DBHelper.TABLE_NAME, contentValues, DBHelper.ROW_ID + " = " + _id, null);
+        return i;
+    }
+
+    public void delete(long _id) {
+        database.delete(DBHelper.TABLE_NAME, DBHelper.ROW_ID + "=" + _id, null);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl102122232/ingetinreminder/Database/DBHelper.java
@@ -0,0 +1,48 @@
+package id.sch.smktelkom_mlg.project.xiirpl102122232.ingetinreminder.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Vistapram on 2016-11-07.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    // Table Name
+    public static final String TABLE_NAME = "kegiatan";
+
+    // Table columns
+    public static final String ROW_ID = "_id";
+    public static final String ROW_NAMAKEG = "nama";
+    public static final String ROW_TGLKEG = "tanggal";
+    public static final String ROW_WAKTU = "waktu";
+    public static final String ROW_FILE = "file";
+    public static final String ROW_KET = "keterangan";
+
+    // Database Information
+    static final String DB_NAME = "reminder.db";
+
+    // database version
+    static final int DB_VERSION = 1;
+
+    // Creating table query
+    private static final String CREATE_TABLE = "CREATE TABLE " + DB_NAME + " ( " +
+            ROW_ID + " VARCHAR, " + ROW_NAMAKEG + " VARCHAR, " + ROW_TGLKEG + " DATE, "  + ROW_WAKTU + " TIME, "  + ROW_FILE + " VARCHAR, " + ROW_KET + " VARCHAR)";
+
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
